4a753d88d757fd692ad41ceaa9dfd0b42761aecf
Moons <qlowds@gmail.com>
2022-10-23 02:35:35 +0100

    Add functionality for creating headpat in db

--- database/query.go
+++ database/query.go
@@ -2,15 +2,30 @@ package database
 
 import (
 	"log"
+	"time"
 
 	"github.com/m-oons/headpat-osc/models"
 )
+
+func CreateHeadpat() bool {
+	headpat := models.Headpat{
+		Time: time.Now(),
+	}
+
+	tx := Connection.Create(&headpat)
+	if tx.Error != nil {
+		log.Printf("Error creating headpat: %v", tx.Error)
+		return false
+	}
+
+	return true
+}
 
 func GetHeadpatCount() int64 {
 	var count int64
 	tx := Connection.Model(&models.Headpat{}).Count(&count)
 	if tx.Error != nil {
-		log.Fatalf("Error getting headpats count: %v", tx.Error)
+		log.Printf("Error getting headpats count: %v", tx.Error)
 	}
 	return count
 }
